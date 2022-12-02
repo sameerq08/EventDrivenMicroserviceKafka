32906994a9a71c8e337b86672b16bb649a19019a
ZI1154 <ZI1154@ZII5021>
2022-12-02 18:28:15 +0530

    Stock Service Added

---
+++ order-service/src/main/java/com/javaguides/orderservice/controller/OrderController.java
@@ -0,0 +1,35 @@
+package com.javaguides.orderservice.controller;
+import com.javaguides.basedomains.dto.Order;
+import com.javaguides.orderservice.kafka.OrderProducer;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import com.javaguides.basedomains.dto.OrderEvent;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1")
+public class OrderController {
+    private OrderProducer orderProducer;
+
+    public OrderController(OrderProducer orderProducer) {
+        this.orderProducer = orderProducer;
+    }
+
+    @PostMapping("/orders")
+    public String placeOrder(@RequestBody Order order){ // RequestBody convert json to java object
+        order.setOrderId(UUID.randomUUID().toString());
+        OrderEvent orderEvent = new OrderEvent();
+        orderEvent.setStatus("PENDING");
+        orderEvent.setMessage("order status is in pending state");
+        orderEvent.setOrder(order);
+
+        orderProducer.sendMessage(orderEvent);
+
+        return "Order place successfully";
+
+
+    }
+}
