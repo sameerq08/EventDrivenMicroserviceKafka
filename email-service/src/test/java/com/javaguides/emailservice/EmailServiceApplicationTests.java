1de87de28dd511a776b16894c23b211706300fb0
ZI1154 <ZI1154@ZII5021>
2022-12-02 12:23:38 +0530

    adding services to git main repository

---
+++ email-service/src/test/java/com/javaguides/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaguides.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
